bede355165b0ee2761306451061ba6a9f3ef43d3
Janelle Klein <janelle@dreamscale.io>
2018-04-26 19:10:15 -0500

    fixed broken build

--- src/main/groovy/org/dreamscale/templates/DreamScaleTemplatesPlugin.groovy
+++ src/main/groovy/org/dreamscale/templates/DreamScaleTemplatesPlugin.groovy
@@ -1,13 +1,11 @@
 package org.dreamscale.templates
 
 import org.dreamscale.templates.tasks.AddJpaObjectTask
-import org.dreamscale.templates.tasks.AddKafkaMessageTask
 import org.dreamscale.templates.tasks.AddRestApiObjectTask
 import org.dreamscale.templates.tasks.CreateBasicResourceTask
 import org.dreamscale.templates.tasks.CreateIntegrationTestProjectTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.dreamscale.templates.tasks.AddKafkaContainerTask
 import org.dreamscale.templates.tasks.AddPostgresContainerTask
 import org.dreamscale.templates.tasks.CreateLibraryProjectTask
 import org.dreamscale.templates.tasks.CreateDeployableProjectTask
@@ -34,8 +32,6 @@ class DreamScaleTemplatesPlugin implements Plugin<Project> {
         project.task 'createCrudResource', type: CreateCrudResourceTask
         project.task 'createBasicResource', type: CreateBasicResourceTask
         project.task 'addPostgresContainer', type: AddPostgresContainerTask
-        project.task 'addKafkaContainer', type: AddKafkaContainerTask
-        project.task 'addKafkaMessage', type: AddKafkaMessageTask
         project.task 'addRestApiObject', type: AddRestApiObjectTask
         project.task 'addJpaObject', type: AddJpaObjectTask
     }
